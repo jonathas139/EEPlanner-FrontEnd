8509dddd010da1fcdaaa742b76ce401ac50bdef6
jonathas <jonathaspereira139@gmail.com>
2024-10-15 20:50:46 -0300

    inicio

---
+++ src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { AuthGuard } from './services/auth-guard.service';
+
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignUpComponent
+    },
+
+    {
+        path: "user",
+        component: UserComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: "principal",
+        component: PrincipalComponent,
+        canActivate: [AuthGuard]
+    }
+];
---
+++ src/app/pages/principal/principal.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  standalone: true,
+  imports: [],
+  templateUrl: './principal.component.html',
+  styleUrl: './principal.component.scss'
+})
+export class PrincipalComponent {
+
+  onSubmit(){
+  
+  }
+}
+
